fe7e09b9b2f6db2c78f232210ef30013c89f3962
durgaprasadraju <durgaprasadraju@gmail.com>
2025-08-30 21:38:42 +0530

    project1

---
+++ project1/lambda/index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+  console.log("Event: ", event);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello, World!',
+    }),
+  };
+};
