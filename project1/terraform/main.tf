8c0fea8a70f5146b7a71482d566f3f03de838fd3
durgaprasadraju <durgaprasadraju@gmail.com>
2025-08-31 11:22:04 +0530

    lambda

--- project1/terraform/main.tf
+++ project1/terraform/main.tf
@@ -2,31 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "project1-lambda-exec-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_cloudwatch_log_group" "lambda_logs" {
-  name              = "/aws/lambda/project1-main-lambda"
-  retention_in_days = 14
-}
-
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../lambda"
@@ -35,9 +10,13 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_lambda_function" "lambda" {
   function_name = "project1-main-lambda"
-  role          = aws_iam_role.lambda_exec.arn
-  handler       = "index.handler"      # adjust for your runtime
-  runtime       = "nodejs18.x"         # or python3.x, etc.
-  filename      = data.archive_file.lambda_zip.output_path
+  
+  # Use your existing IAM role ARN here (replace below)
+  role          = "arn:aws:iam::381492023386:role/project1-lambda-exec-role"
+  
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+  
+  filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
